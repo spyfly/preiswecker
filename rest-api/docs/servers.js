a548fc02a476b1ac751598c26d33bb70f8cd990c
Sebastian Heiden <seb.heiden@gmail.com>
2022-01-17 21:13:51 +0100

    Add Production to rest-api docs

--- rest-api/docs/servers.js
+++ rest-api/docs/servers.js
@@ -4,5 +4,9 @@ module.exports = {
         url: "http://localhost:5000/api", // url
         description: "Local server", // name
       },
+      {
+        url: "http://preiswecker.spyfly.xyz/api", // url
+        description: "Production", // name
+      },
     ],
   };
