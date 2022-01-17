a91413c60a7db9e7db355f278523c4fa8812a8ee
Sebastian Heiden <seb.heiden@gmail.com>
2022-01-17 20:27:44 +0100

    Preiswecker Extension (only lacking login)

--- browser-extension/preiswecker.user.js
+++ browser-extension/preiswecker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geizhals.de Preiswecker
 // @namespace    https://preiswecker.spyfly.xyz
-// @version      0.1
+// @version      0.2
 // @description  Allows adding Preisweckers directly from geizhals.de
 // @author       Sebastian Heiden
 // @match        https://geizhals.de/?cat=*
@@ -10,7 +10,7 @@
 
 (function() {
     /* Store Auth Token in extension (requires frontend support first) */
-    const userToken = null;
+    const userToken = "";
 
     'use strict';
     /* Preiswecker hinzufügen Button */
@@ -20,6 +20,7 @@
     a.style = "cursor: pointer;";
     a.id = "preiswecker_btn";
     let div = document.createElement("div");
+    let dialogModified = false;
     div.className = "sorting";
     div.append(a);
     document.querySelector(".sorting-paginator__wrapper").prepend(div);
@@ -33,7 +34,72 @@
                 GM.openInTab("https://preiswecker.spyfly.xyz/login", false);
             }
         } else {
+            /* Modify Dialog */
+            document.querySelector("#dialog-filters-help").style = "display: flex";
+            if (!dialogModified) {
+                dialogModified = true;
+                document.querySelector("#dialog-filters-help__dialog__headline__title").innerText = "Preiswecker hinzufügen";
+                document.querySelector(".dialog__content ").innerHTML = `<div class="dialog__content__section">
+                    <div class="textfield full-width">
+                        <label for="pa_name" class="textfield__label">Name für den Preiswecker:</label>
+                        <input id="pa_name" type="text" class="textfield__input" placeholder=" Nintendo Switch">
+                    </div>
+                    <div class="textfield">
+                        <label for="pa_limit" class="textfield__label">Preislimit:</label>
+                        <input id="pa_limit" type="number" min="0" inputmode="decimal" class="textfield__input" placeholder=" 5,46">
+                    </div>
+                </div>
+                <div class="dialog__action dialog__action--fixed">
+                    <button id="pa_save" class="button">
+                        Speichern
+                    </button>
+                    <button type="button" class="dialog--close button button--outlined">
+                        Abbrechen
+                   </button>
+                </div>`;
 
+                //Hide Dialog when close btn is clicked
+                for (const closeBtn of document.querySelectorAll(".dialog--close")) {
+                    closeBtn.addEventListener("click", () => {
+                        //Hide Dialog
+                        document.querySelector("#dialog-filters-help").style = "";
+                    })
+                }
+
+                //Init Save Button
+                document.querySelector("#pa_save").addEventListener("click", async () => {
+                    //Hide Dialog
+                    document.querySelector("#dialog-filters-help").style = "";
+
+                    //Build Req
+                    let postBody = new URLSearchParams();
+                    postBody.append("name", document.querySelector("#pa_name").value);
+                    postBody.append("filterUrl", window.location.href);
+                    postBody.append("targetPrice", document.querySelector("#pa_limit").value);
+
+                    fetch("https://preiswecker.spyfly.xyz/api/user/pricealert", {
+                        method: "POST",
+                        headers: {
+                            "Authorization": "Bearer " + userToken,
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        },
+                        body: postBody.toString()
+                    }).then(async function(response) {
+                        const json = await response.json();
+                        if (json.msg) {
+                            alert(json.msg);
+                        } else {
+                            let errors = [];
+                            for (const error of json.errors) {
+                                errors.push(error.msg);
+                            }
+                            alert(errors.length + " Errors occured:\n - " + errors.join("\n - "))
+                        }
+                    }).catch(function(error) {
+                        console.log(error);
+                    });
+                });
+            }
         }
     });
 })();
