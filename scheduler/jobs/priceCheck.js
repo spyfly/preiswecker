6aa48d41069d720021b34f5570da58781954c1c5
Sebastian Heiden <seb.heiden@gmail.com>
2022-01-17 20:49:30 +0100

    Use HTTP Protocol (we didn't invent our own yet)

--- scheduler/jobs/priceCheck.js
+++ scheduler/jobs/priceCheck.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 (async () => {
-    axios.get('rest-api://rest-api:5000/api/pricealerts')
+    axios.get('http://rest-api:5000/api/pricealerts')
         .then(function (responseApi) {
             if(responseApi.data !== undefined && responseApi.data !== null){
                 console.log(responseApi.data);
                 for(const user of responseApi.data.priceAlertsFromUsers){
                     for(const priceAlert of user.priceAlerts){
-                        axios.get('crawler://crawler:3000/fetch' + priceAlert.filterUrl.replace("https://geizhals.de", "").replace("https://geizhals.at", "").replace("https://geizhals.eu", ""))
+                        axios.get('http://crawler:3000/fetch' + priceAlert.filterUrl.replace("https://geizhals.de", "").replace("https://geizhals.at", "").replace("https://geizhals.eu", ""))
                         .then(function (responseCrawler) {
                             if(responseCrawler.data !== undefined && responseCrawler.data !== null){
                                 console.log(responseCrawler.data);
@@ -21,7 +21,7 @@ const axios = require('axios');
                                         }
                                     }
                                 }
-                                axios.put(`rest-api://rest-api:5000/api/pricealerts/${priceAlert._id}`, {userID: user.userID, reachedPrice: reachedPrice, reached: reached })
+                                axios.put(`http://rest-api:5000/api/pricealerts/${priceAlert._id}`, {userID: user.userID, reachedPrice: reachedPrice, reached: reached })
                                 .then(function (responseApi2) {
                                     if(responseApi2.status === 200){
                                         console.log(`Price alert with ID ${priceAlert._id} by user with ID ${user.userID} with the following price: ${reachedPrice} and the status ${reached} updated!.`);
