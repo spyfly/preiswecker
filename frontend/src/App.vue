936d5e2952af35ad8956972086dae21ef24482ad
kevinunger <kevinunger98@gmail.com>
2022-01-25 14:38:30 +0100

    +token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="app">
+<div id="app" :data-token=token>
   <Header/>
   <div id="content">
     <router-view/>
@@ -8,23 +8,38 @@
 </template>
 <script>
 import Header from './components/Header.vue'
+
 export default {
   name: '',
+  data() {
+    return {
+    }
+  },
   components:{
     Header
   },
   methods:{
-    cb(){
-      
+  },
+  computed:{
+    token(){
+      return this.$store.state.accessToken;
     }
   }
+
 }
 </script>
 <style>
 @import url('https://fonts.googleapis.com/css?family=Roboto');
 
+#getToken{
+  display: none;
+}
+
 html, body {
   font-family: 'Roboto', sans-serif;
+  background-color: #000000;
+  background-image: linear-gradient(315deg, #000000 0%, #414141 74%);
+
 }
 
 #app {
@@ -35,7 +50,7 @@ html, body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 #content{
   margin: 50px 20px;
