a137bdd06c5699278d747effbb3a366b3209aeff
kevinunger <kevinunger98@gmail.com>
2022-01-25 14:41:59 +0100

    +bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 import router from './router'
 import store from "./store/store";
 
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
 /* axios */
 import axios from 'axios'
@@ -15,17 +20,22 @@ import VueAxios from 'vue-axios'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { VuesticPlugin } from 'vuestic-ui'
-import 'vuestic-ui/dist/vuestic-ui.css'
+
+
 
 
 library.add(fas);
+
+const config = {
+    colors: { 'primary': '#A3D39C', 'secondary': '#3D3D3D', 'abort' : '#E45F56'},
+  }
 
 
 createApp(App)
     .use(router)
     .component("fa", FontAwesomeIcon)
-    .use(VuesticPlugin)
+    .use(VuesticPlugin, config)
     .use(VueAxios, axios)
     .use(store)
+    .use(BootstrapVue3)
     .mount('#app')
